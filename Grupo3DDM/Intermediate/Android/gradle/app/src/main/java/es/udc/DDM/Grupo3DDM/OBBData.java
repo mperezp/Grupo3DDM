1eea1c04e6fbb72d8c8cece15d7086a3c0d4df16
KDK666 <randomgameplayer1234@gmail.com>
2022-06-19 21:02:32 +0200

    GPS, archivements y balaceo teoricamente funcional

--- Grupo3DDM/Intermediate/Android/gradle/app/src/main/java/es/udc/DDM/Grupo3DDM/OBBData.java
+++ Grupo3DDM/Intermediate/Android/gradle/app/src/main/java/es/udc/DDM/Grupo3DDM/OBBData.java
@@ -18,7 +18,7 @@ mFileSize = fileSize;
 public static final XAPKFile[] xAPKS = {
 new XAPKFile(
 true, // true signifies a main file
-"1", // the version of the APK that the file was uploaded against
+"2", // the version of the APK that the file was uploaded against
 0L // the length of the file in bytes
 )
 };
